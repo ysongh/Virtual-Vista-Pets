d3463b53aa9995468299f85606ff157754bee2d5
You Song Hou <ysongweb3@gmail.com>
2024-09-16 01:49:29 -0400

    feat: replace RainbowKit with WalletConnect AppKit in the header

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -1,12 +1,11 @@
-"use client"
-import Image from "next/image";
+"use client";
+
 import Link from 'next/link'
-import {
-  useAccountModal,
-} from '@rainbow-me/rainbowkit';
+import { useWeb3ModalAccount } from "@web3modal/ethers/react";
 
 const Header = () => {
-  const { openAccountModal } = useAccountModal();
+  const { isConnected } = useWeb3ModalAccount();
+
   return (
     <div className="fixed top-0 w-full bg-blue-600 h-20 flex items-center z-10">
       <div className="relative left-4">
@@ -14,7 +13,7 @@ const Header = () => {
           <p className="text-white text-2xl">Virtual Vista Pets</p>
         </Link>
       </div>
-      {openAccountModal && <div className="fixed right-6">
+      {isConnected && <div className="fixed right-6">
         <div className="flex flex-row gap-8 items-center">
           <Link href="/defi">
             <div className="text-white font-bold">
@@ -27,16 +26,7 @@ const Header = () => {
             </div>
           </Link>
           <div className="text-black items-center inline-flex bg-white border-2 border-black duration-200 ease-in-out focus:outline-none hover:bg-black hover:shadow-none hover:text-white justify-center rounded-[20px] shadow-[5px_5px_black] text-center transform transition w-full lg:px-2 lg:py-2 lg:text-xl px-2 py-2">
-
-            <button onClick={openAccountModal} type="button">
-              <Image
-                className="cursor-pointer "
-                src="/assets/icons/fvm.png"
-                width={30}
-                height={30}
-                alt="FVM Logo"
-              />
-            </button>
+            <w3m-button />
           </div>
         </div>
       </div>
