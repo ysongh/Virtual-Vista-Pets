7fc36fbd1bb28b101baab71582c6d367d168a5f1
You Song Hou <ysongweb3@gmail.com>
2024-09-22 01:06:04 -0400

    refactor: update the footer

--- frontend/app/components/footer.tsx
+++ frontend/app/components/footer.tsx
@@ -2,13 +2,8 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="bottom-0 w-full bg-blue-600 flex items-center flex-col lg:flex-row lg:justify-between gap-4 px-4">
-      <div className="flex flex-row gap-8 items-center justify-center mb-4">
-       <p className="text-white text-2xl">Virtual Vista Pets</p>
-      </div>
-      <div className="flex flex-row gap-2 justify-center items-center mb-2">
-        <p className="text-white text-2xl">2024</p>
-      </div>
-    </div>
+    <footer className="bg-purple-900 text-center p-6 mt-8">
+      <p>&copy; 2024 Virtual Vista Pets. All rights reserved.</p>
+    </footer>
   );
 }
