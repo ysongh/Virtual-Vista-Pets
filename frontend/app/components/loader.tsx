2f6a66930d69ef3e4b2e7f42f9b86ecd64fa8982
You Song Hou <ysongweb3@gmail.com>
2024-10-02 22:45:29 -0400

    feat: update the loading indicator

--- frontend/app/components/loader.tsx
+++ frontend/app/components/loader.tsx
@@ -1,9 +1,21 @@
 export default function Loader() {
     return (
-        <div className="flex flex-row gap-2">
-            <div className="w-4 h-4 rounded-full bg-yellow-300 animate-bounce [animation-delay:.7s]"></div>
-            <div className="w-4 h-4 rounded-full bg-yellow-300 animate-bounce [animation-delay:.3s]"></div>
-            <div className="w-4 h-4 rounded-full bg-yellow-300 animate-bounce [animation-delay:.7s]"></div>
+        <div className="flex flex-col items-center justify-center">
+            <div className="relative mb-4">
+                {[...Array(3)].map((_, index) => (
+                <div
+                    key={index}
+                    className={`absolute inset-0 rounded-full border-2 border-transparent border-t-yellow-500 border-r-yellow-500
+                                animate-spin`}
+                    style={{
+                    animationDuration: '1.5s',
+                    animationDelay: `${index * 0.2}s`,
+                    width: `${(index + 3) * 12}px`,
+                    height: `${(index + 3) * 12}px`,
+                    }}
+                ></div>
+                ))}
+            </div>
         </div>
     );
 }
