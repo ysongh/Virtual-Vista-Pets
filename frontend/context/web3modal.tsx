f5653719449f6fab4e9a7da340b7670501f57de6
You Song Hou <ysongweb3@gmail.com>
2024-10-10 22:10:52 -0400

    feat: add a custom network to AppKit

--- frontend/context/web3modal.tsx
+++ frontend/context/web3modal.tsx
@@ -1,8 +1,35 @@
 'use client'
 
-import { createAppKit } from '@reown/appkit/react'
-import { EthersAdapter } from '@reown/appkit-adapter-ethers'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { createAppKit } from '@reown/appkit/react';
+import { EthersAdapter } from '@reown/appkit-adapter-ethers';
+import { mainnet, arbitrum } from '@reown/appkit/networks';
+import { defineChain } from '@reown/appkit/networks';
+import { filecoinCalibration } from 'wagmi/chains';
+
+// Define the custom network
+const filecoinCalibrationNetwork = defineChain({
+  id: filecoinCalibration.id,
+  caipNetworkId: 'eip155:' + filecoinCalibration.id,
+  chainNamespace: 'eip155',
+  name: 'Calibration',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'testnet filecoin',
+    symbol: 'tFIL',
+  },
+  rpcUrls: {
+    default: {
+      http: [filecoinCalibration.rpcUrls.default.http],
+      webSocket: ['WS_RPC_URL'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://calibration.filfox.info/en' },
+  },
+  contracts: {
+    // Add the contracts here
+  }
+})
 
 // 1. Get projectId at https://cloud.reown.com
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
@@ -19,7 +46,7 @@ const metadata = {
 createAppKit({
   adapters: [new EthersAdapter()],
   metadata,
-  networks: [mainnet, arbitrum],
+  networks: [mainnet, arbitrum, filecoinCalibrationNetwork],
   projectId,
   features: {
     analytics: true // Optional - defaults to your Cloud configuration
